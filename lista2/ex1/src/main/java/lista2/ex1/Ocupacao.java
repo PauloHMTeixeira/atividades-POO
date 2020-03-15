6d35ddee27f87e2e57a26c6e78f419c4b85a2184
Paulo Teixeira <49165506+PauloHMTeixeira@users.noreply.github.com>
2020-03-15 18:12:01 -0300

    Update Ocupacao.java

--- lista2/ex1/src/main/java/lista2/ex1/Ocupacao.java
+++ lista2/ex1/src/main/java/lista2/ex1/Ocupacao.java
@@ -2,4 +2,14 @@ package main.java.lista2.ex1;
 
 public class Ocupacao{
     public String descricao;
+    public int id;
+    @Override
+        public boolean equals(Object o){
+            if(this.id == ((Pessoa)o).id){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
 }
