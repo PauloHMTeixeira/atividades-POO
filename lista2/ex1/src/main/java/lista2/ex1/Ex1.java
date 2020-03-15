568fa4ed72f012a499fba84de5e3fa015451d6cd
Paulo Teixeira <49165506+PauloHMTeixeira@users.noreply.github.com>
2020-03-15 18:11:38 -0300

    Update Ex1.java

--- lista2/ex1/src/main/java/lista2/ex1/Ex1.java
+++ lista2/ex1/src/main/java/lista2/ex1/Ex1.java
@@ -6,21 +6,40 @@ import java.math.BigDecimal;
 
 public class Ex1{
     public static void main(String[] args){
-        Aluno p1 = new Aluno("Paulo", 19, "07810176455");
+        try{
+        Aluno p1 = new Aluno("Paulo", 19, "111");
         p1.curso = "CC";
         p1.matriculaTrancada = false;
         System.out.println(p1);
 
-        Responsavel p2 = new Responsavel("Roberto", 45, "2222222222");
+        Aluno p4 = new Aluno("Serro", 21, "333");
+        p4.curso = "CC";
+        p4.matriculaTrancada = false;
+        System.out.println(p4);
+
+        Responsavel p2 = new Responsavel("Roberto", 45, "222");
         p2.aluno = p1;
         p2.renda = new BigDecimal(10000d);
         p2.ocupacao = new Ocupacao();
         p2.ocupacao.descricao = "Adm";
+        p2.ocupacao.id = 123;
         System.out.println(p2);
 
-        p1.equals(p2);
-        System.out.println(p1.equals(p2));
+        Responsavel p3 = new Responsavel("Sergio", 53, "444");
+        p3.aluno = p4;
+        p3.renda = new BigDecimal(10000d);
+        p3.ocupacao = new Ocupacao();
+        p3.ocupacao.descricao = "Juiz";
+        p3.ocupacao.id = 321;
+        System.out.println(p3);
+
+        p1.equals(p4);
+        System.out.println(p1.equals(p4));
+        p2.equals(p3);
+        System.out.println(p2.equals(p3));
         p1.hashCode();
         System.out.println(p1.hashCode());
+        }
+        catch(Exception e ) {}
     }
 }
